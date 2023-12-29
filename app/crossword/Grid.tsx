125e18242867878123c3087b673f50b70cc31ac4
Scott Newman <snewman91@gmail.com>
2023-12-29 18:15:34 -0500

    Update title font and use react state in grid component.

--- app/crossword/Grid.tsx
+++ app/crossword/Grid.tsx
@@ -1,6 +1,11 @@
+'use client'
+
 import Cell from './Cell';
 
+import { useState } from 'react'
+
 export default function Grid() {
+    const [gridSize, setGridSize] = useState({width: 15, height: 15});
     return (
         <Cell />
     );
