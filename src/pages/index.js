6207d0b37ccbec5842e2ec0423583160ac4abf54
Scott Newman <scooternew@Scotts-Air.lan>
2023-12-29 13:15:19 -0500

    Remove old info and remove personal image.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,10 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h2 style={{textAlign: `center`}}>Hi - I&#39;m Scott. Sometimes people call me Scotty.</h2>
-    <p>I&#39;m a professional software developer based in Queens, New York. I spent the past 6 years at Google, specializing
-        in building highly concurrent Android applications with a focus on speech recognition, location acquisition,
-        and user context.
+    <h2 style={{textAlign: `center`}}>Hi - I&#39;m Scott.</h2>
+    <p>I&#39;m a professional software developer based in Queens, New York.
     </p>
-    <p>More to come soon - stay tuned!</p>
-    <div style={{ maxWidth: `500px`, marginBottom: `1.45rem`, margin: `0 auto`}}>
-      <Image />
-    </div>
+    <p>More to come soon!</p>
   </Layout>
 )
 
