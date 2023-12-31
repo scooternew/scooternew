fb5d7dcd076106e7bb1117e7c8d94425abf42670
Scott Newman <snewman91@gmail.com>
2023-12-30 23:15:24 -0500

    Add no-op login button and fix page styling.

--- app/crossword/page.tsx
+++ app/crossword/page.tsx
@@ -1,20 +1,35 @@
+"use client"
+
 import Grid from "./Grid"
 
 export default function Crossword() {
   console.log("Creating crossword window...")
+
+  const handleLoginButtonClick = (event: React.MouseEvent) => {
+    console.log("Page level - login button clicked")
+    console.log(event)
+  }
   return (
     <div
       style={{
         paddingLeft: "25px",
         paddingTop: "25px",
-        width: "700px",
-        height: "600px",
+        width: "80%",
+        height: "80%",
         backgroundColor: "gray",
       }}
       tabIndex={-1}
     >
       <Grid />
       Grid size will be adjustable here.
+      <div>
+        <button
+          onClick={handleLoginButtonClick}
+          style={{ border: "2px solid red" }}
+        >
+          Login button goes here
+        </button>
+      </div>
     </div>
   )
 }
