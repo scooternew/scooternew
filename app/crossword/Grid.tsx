8b2cc8392f727ac2d892149cca90167e5f88c5a6
Scott Newman <snewman91@gmail.com>
2024-01-01 05:01:07 -0500

    Move keyboard handling to window level and create grid model.

--- app/crossword/Grid.tsx
+++ app/crossword/Grid.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import React, { ReactComponentElement } from "react"
 import Cell from "./Cell"
 
 import { ReactNode, useState, useEffect } from "react"
@@ -12,7 +13,7 @@ enum Direction {
 export interface GridProps {
   width: number
   height: number
-  cells: string[]
+  // cells: string[]
 }
 
 export default function Grid() {
@@ -23,52 +24,20 @@ export default function Grid() {
   const paddingBottomPercent = 100 / gridSize + "%"
 
   // TODO(scooternew): Externalize as global or top-level constants.
-  const [selectedX, setSelectedX] = useState(0)
-  const [selectedY, setSelectedY] = useState(0)
+  const [cursorPosition, setCursorPosition] = useState([0, 0])
   const [direction, setDirection] = useState(Direction.Horizontal)
-  // const [cellList, setCellList] = useState([])
-
+  const [blockList, setBlockList] = useState({})
   // TODO(scooternew): Model as map or 2d array.
-  const cells = []
 
-  // TODO(scooternew): Move into separate functions.
-  const handleKeyDown = (event: React.KeyboardEvent) => {
-    // console.log(event)
-    if (event.key === "ArrowLeft") {
-      console.log("Left arrow was pressed")
-      setSelectedX(selectedX == 0 ? gridSize - 1 : selectedX - 1)
-      setDirection(Direction.Horizontal)
-    }
-    if (event.key === "ArrowRight") {
-      console.log("Right arrow was pressed")
-      setSelectedX(selectedX == gridSize - 1 ? 0 : selectedX + 1)
-      setDirection(Direction.Horizontal)
-    }
-    if (event.key === "ArrowUp") {
-      console.log("Up arrow was pressed")
-      setSelectedY(selectedY == 0 ? gridSize - 1 : selectedY - 1)
-      setDirection(Direction.Vertical)
-    }
-    if (event.key === "ArrowDown") {
-      console.log("Down arrow was pressed")
-      setSelectedY(selectedY == gridSize - 1 ? 0 : selectedY + 1)
-    }
-    if (event.key === ".") {
-      console.log("Grid level event handler - period key was pressed")
-      // setIsBlock(!isBlock)
-    }
-  }
-
-  console.log("Grid - selected X: " + selectedX)
-  console.log("Grid - selected Y: " + selectedY)
+  const cellsList = []
 
   // TODO(scooternew): Support default props.
   for (var i = 0; i < gridSize * gridSize; i++) {
     const curX = i % gridSize
     const curY = Math.floor(i / gridSize)
-    let curSelected = curX == selectedX && curY == selectedY
+    let curSelected = curX == cursorPosition[0] && curY == cursorPosition[1]
 
-    cells.push(
+    cellsList.push(
       <Cell
         {...{
           selected: curSelected,
@@ -82,22 +51,88 @@ export default function Grid() {
     )
   }
 
+  const [grid, setGrid] = useState(cellsList)
+
+  // TODO(scooternew): Forward ref?
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown)
+
+    // clean up
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [])
+
+  // TODO(scooternew): Move into separate functions.
+  const handleKeyDown = (event: any) => {
+    console.log(event)
+    const cellIndexX = cursorPosition[0]
+    const cellIndexY = cursorPosition[1]
+    if (event.key === "ArrowLeft") {
+      console.log("Left arrow was pressed")
+      setCursorPosition((cell) => [
+        cell[0] == 0 ? gridSize - 1 : cell[0] - 1,
+        cell[1],
+      ])
+      setDirection(Direction.Horizontal)
+    }
+    if (event.key === "ArrowRight") {
+      console.log("Right arrow was pressed")
+      setCursorPosition((cell) => [
+        cell[0] == gridSize - 1 ? 0 : cell[0] + 1,
+        cell[1],
+      ])
+      setDirection(Direction.Horizontal)
+    }
+    if (event.key === "ArrowDown") {
+      console.log("Up arrow was pressed")
+      setCursorPosition((cell) => [
+        cell[0],
+        cell[1] == gridSize - 1 ? 0 : cell[1] + 1,
+      ])
+      setDirection(Direction.Vertical)
+    }
+    if (event.key === "ArrowUp") {
+      console.log("Down arrow was pressed")
+      setCursorPosition((cell) => [
+        cell[0],
+        cell[1] == 0 ? gridSize - 1 : cell[1] - 1,
+      ])
+      setDirection(Direction.Vertical)
+    }
+    if (event.key === ".") {
+      console.log("Period key was pressed")
+      const map = { test: "Hello!" }
+      setBlockList(map)
+    }
+  }
+
+  console.log(
+    "Selected cell: [" + cursorPosition[0] + ", " + cursorPosition[1] + "]"
+  )
+
+  console.log("CellsList: " + cellsList)
+  console.log("Grid: " + grid)
+  console.log("Blocklist: " + blockList)
+
   // TODO(scooternew): Handle events at top-level and propagate downward.
   // TODO(scooternew): Debounce events.
   return (
-    <div
-      className="grid"
-      onKeyDown={handleKeyDown}
-      style={{
-        display: "table-cell", // TODO(scooternew): should be "block" or "inline-block"
-        border: "5px solid black",
-        width: gridSizePx,
-        height: gridSizePx,
-        backgroundColor: "gray",
-      }}
-      tabIndex={-1} // Ensures this can respond to key events.
-    >
-      {...cells}
-    </div>
+    <React.StrictMode>
+      <div
+        className="grid"
+        onKeyDown={handleKeyDown}
+        style={{
+          display: "table-cell", // TODO(scooternew): should be "block" or "inline-block"
+          border: "5px solid black",
+          width: gridSizePx,
+          height: gridSizePx,
+          backgroundColor: "gray",
+        }}
+        tabIndex={-1} // Ensures this can respond to key events.
+      >
+        {...cellsList}
+      </div>
+    </React.StrictMode>
   )
 }
--- app/crossword/page.tsx
+++ app/crossword/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import React from "react"
 import Grid from "./Grid"
 
 export default function Crossword() {
@@ -10,26 +11,28 @@ export default function Crossword() {
     console.log(event)
   }
   return (
-    <div
-      style={{
-        paddingLeft: "25px",
-        paddingTop: "25px",
-        width: "80%",
-        height: "80%",
-        backgroundColor: "gray",
-      }}
-      tabIndex={-1}
-    >
-      <Grid />
-      Grid size will be adjustable here.
-      <div>
-        <button
-          onClick={handleLoginButtonClick}
-          style={{ border: "2px solid red" }}
-        >
-          Login button goes here
-        </button>
+    <React.StrictMode>
+      <div
+        style={{
+          paddingLeft: "25px",
+          paddingTop: "25px",
+          width: "80%",
+          height: "80%",
+          backgroundColor: "gray",
+        }}
+        tabIndex={-1}
+      >
+        <Grid />
+        Grid size will be adjustable here.
+        <div>
+          <button
+            onClick={handleLoginButtonClick}
+            style={{ border: "2px solid red" }}
+          >
+            Login button goes here
+          </button>
+        </div>
       </div>
-    </div>
+    </React.StrictMode>
   )
 }
