b051ad47b18c6fde4deab2b30f52476e37ef3e38
Scott Newman <snewman91@gmail.com>
2024-09-21 16:26:38 -0400

    update everything

--- app/1994-09-22/page.tsx
+++ app/1994-09-22/page.tsx
@@ -2,21 +2,35 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        A little birthday puzzle,
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <i>
+        A little birthday puzzle
+        <br />
         To help the aging mind;
-        Bring order to the colors
+        <br />
+        Color gives the order
+        <br />
         In frequencies confined
-
-        
-
-        
-        <a href="https://connections.swellgarfo.com/game/-O7KsBQpiVayzyIOlEEH">
-          Culinary Caper
-        </a>
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"></p>
-      </div>
+        <br />
+        <br />
+        Titles give the pieces
+        <br />
+        From Categories bound
+        <br />
+        Four times take the first
+        <br />
+        And a present will be found
+      </i>
+      <br />
+      www.scooternew.com/?-?-?-?
+      <br />
+      <br />
+      <a
+        href="https://connections.swellgarfo.com/game/-O7KsBQpiVayzyIOlEEH"
+        style={{ fontSize: "2em" }}
+      >
+        Culinary Caper
+      </a>
     </main>
   )
 }
